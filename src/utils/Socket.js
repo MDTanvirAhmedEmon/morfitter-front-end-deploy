27faa0cb13c59146480906d9ad85d619f726f311
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-09-20 12:50:49 +0600

    update links

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io('http://localhost:5000/live-chats');
+export const socket = io('https://api.morfitter.com/live-chats');
--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,2 +1,2 @@
-export const chatsUrl = 'http://localhost:5002/live-chats'
-// export const chatsUrl = 'https://api.morfitter.com/live-chats'
+// export const chatsUrl = 'http://localhost:5002/live-chats'
+export const chatsUrl = 'https://api.morfitter.com/live-chats'
